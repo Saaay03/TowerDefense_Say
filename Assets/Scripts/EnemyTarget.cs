16166d79a7a48f9d71ffe57377f3ea7dbfe59e95
SaySanchez <sayra.sanchez@uteca.edu.mx>
2025-08-04 18:09:46 -0600

    Actualizacion

---
+++ Assets/Scripts/EnemyTarget.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+[System.Serializable]
+
+public class EnemyTarget
+{
+    private Transform _target;
+    public Transform Target => _target;
+    private Health _health;
+    public Health Health => _health;
+    public EnemyTarget(Transform target, Health health)
+    {
+        _target = target;
+        _health = health;
+    }
+}
